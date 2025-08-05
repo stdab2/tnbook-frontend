8e5a7145c3a637b387e05c470c0fc9eccae09cad
Steeve Dabire <steeve.dabire.1@ulaval.ca>
2025-08-05 06:10:27 -0400

    styling + landing page atrted

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavBar } from './shared/nav-bar/nav-bar'
 import { initFlowbite } from 'flowbite';
+import { LandingPage } from "./pages/landing-page/landing-page";
 
 @Component({
   selector: 'app-root',
-  imports: [NavBar],
+  imports: [NavBar, LandingPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
